01d456d53eae7549f5355c6b2327af6b250c27f1
mariakesa <maria.kesa@gmail.com>
2023-02-10 20:07:45 +0200

    bug fixes

--- loader.py
+++ loader.py
@@ -5,6 +5,10 @@ from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler, random_sp
 import glob
 import cv2
 from tqdm import tqdm
+import sys
+sys.path.insert(0, "/home/maria/Desktop/t-simcne")
+from tsimcne.tsimcne import TSimCNE
+from tsimcne.imagedistortions import TransformedPairDataset, get_transforms
 
 class GeneralSurgeryToolsDataset(Dataset):
     def __init__(self):
@@ -34,23 +38,50 @@ class GeneralSurgeryToolsDataset(Dataset):
         img = cv2.imread(img_path)
         img = cv2.resize(img, self.img_dim)
         class_id = self.class_map[class_name]
-        img_tensor = torch.from_numpy(img)
-        img_tensor = img_tensor.permute(2, 0, 1)
-        class_id = torch.tensor([class_id])
-        return img_tensor, class_id
+        #img_tensor = torch.from_numpy(img)
+        #img_tensor = img_tensor.permute(2, 0, 1).numpy()
+        class_id = torch.tensor([class_id]).numpy()
+        from PIL import Image
+        #im=Image.fromarray(img_tensor)
+        #print(img.shape)
+        img=torch.Tensor(img.transpose(2,0,1))
+        #img=Image.fromarray(img)
+        return img, class_id
 
-def calc_mean_std(train_loader):
-    # placeholders
-    psum    = torch.tensor([0.0, 0.0, 0.0])
-    psum_sq = torch.tensor([0.0, 0.0, 0.0])
+def calculate_mean_std(dataloader):
+    mean = torch.zeros(3).to(torch.float32)
+    variance = torch.zeros(3).to(torch.float32)
+    num_samples = 0
 
-    # loop through images
-    for inputs in tqdm(train_loader):
-        #print(inputs)
-        psum    += inputs[0].sum(axis        = [0, 2, 3])
-        psum_sq += (inputs[0] ** 2).sum(axis = [0, 2, 3])
-    size = (100, 100)
-    return psum, np.sqrt(psum_sq), size
+    for data, _ in dataloader:
+        # Get the data tensor from the batch
+        data = data[0]
+
+        # Sum up the values of each channel
+        mean[0] += data[..., 0].mean().item()
+        mean[1] += data[..., 1].mean().item()
+        mean[2] += data[..., 2].mean().item()
+
+        # Sum up the squared values of each channel
+        variance[0] += data[..., 0].pow(2).mean().item()
+        variance[1] += data[..., 1].pow(2).mean().item()
+        variance[2] += data[..., 2].pow(2).mean().item()
+
+        # Increment the number of samples
+        num_samples += data.shape[0]
+
+    # Divide the sum by the number of samples to get the mean
+    mean /= num_samples
+
+    # Divide the sum of squared values by the number of samples and subtract the square of the mean to get the variance
+    variance /= num_samples
+    variance -= mean.pow(2)
+
+    # Take the square root of the variance to get the standard deviation
+    std = torch.sqrt(variance)
+
+    return mean, std
+
 
 def generate_loaders():
     train_set = GeneralSurgeryToolsDataset()
@@ -66,6 +97,62 @@ def generate_loaders():
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
     validation_loader = torch.utils.data.DataLoader(valid_set, batch_size=batch_size)
     
-    mean, std, size= calc_mean_std(train_loader)
+    mean, std = calculate_mean_std(train_loader)
 
-    return {'train_loader':train_set,'validation_loader':valid_set,'mean':mean,'std':std,'size':size}
+    return {'train_loader':train_set,'validation_loader':valid_set,'mean':mean,'std':std,'size':(100,100)}
+
+dct=generate_loaders()
+
+train_loader=dct['train_loader']
+validation_loader=dct['validation_loader']
+mean=dct['mean']
+std=dct['std']
+size=dct['size']
+
+dataset_full = torch.utils.data.ConcatDataset(
+    [train_loader, validation_loader]
+)
+
+# data augmentations for contrastive training
+transform = get_transforms(
+    mean,
+    std,
+    size=size,
+    setting="contrastive",
+)
+# transform_none just normalizes the sample
+transform_none = get_transforms(
+    mean,
+    std,
+    size=size,
+    setting="test_linear_classifier",
+)
+
+# datasets that return two augmented views of a given datapoint (and label)
+dataset_contrastive = TransformedPairDataset(train_loader, transform)
+dataset_visualize = TransformedPairDataset(dataset_full, transform_none)
+
+# wrap dataset into dataloader
+train_dl = torch.utils.data.DataLoader(
+    dataset_contrastive, batch_size=30, shuffle=True
+)
+orig_dl = torch.utils.data.DataLoader(
+    dataset_visualize, batch_size=30, shuffle=False
+)
+
+import time
+
+start=time.time()
+# create the object
+tsimcne = TSimCNE(total_epochs=[10, 50, 250])
+# train on the augmented/contrastive dataloader (this takes the most time)
+tsimcne.fit(train_dl)
+# fit the original images
+Y, labels = tsimcne.transform(orig_dl)
+end=time.time()
+
+print('Time to train: ', end-start)
+import pickle
+with open('filename.pickle', 'wb') as handle:
+    pickle.dump({'Y':Y,'labels':labels}, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
