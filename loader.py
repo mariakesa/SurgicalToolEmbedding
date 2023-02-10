5cd10544848c1e6075d284300321aa87fbea4fbb
mariakesa <maria.kesa@gmail.com>
2023-02-10 04:20:06 +0200

    cleared up a lot of bugs

--- loader.py
+++ loader.py
@@ -1,9 +1,10 @@
 import numpy as np
 import torch
 import os
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler, random_split
 import glob
 import cv2
+from tqdm import tqdm
 
 class GeneralSurgeryToolsDataset(Dataset):
     def __init__(self):
@@ -21,7 +22,7 @@ class GeneralSurgeryToolsDataset(Dataset):
                 cn.append(class_name)
         self.class_map = {}
         i=0
-        for c in cn:
+        for c in set(cn):
             self.class_map[c]=i
             i+=1
         print(self.class_map)
@@ -37,3 +38,34 @@ class GeneralSurgeryToolsDataset(Dataset):
         img_tensor = img_tensor.permute(2, 0, 1)
         class_id = torch.tensor([class_id])
         return img_tensor, class_id
+
+def calc_mean_std(train_loader):
+    # placeholders
+    psum    = torch.tensor([0.0, 0.0, 0.0])
+    psum_sq = torch.tensor([0.0, 0.0, 0.0])
+
+    # loop through images
+    for inputs in tqdm(train_loader):
+        #print(inputs)
+        psum    += inputs[0].sum(axis        = [0, 2, 3])
+        psum_sq += (inputs[0] ** 2).sum(axis = [0, 2, 3])
+    size = (100, 100)
+    return psum, np.sqrt(psum_sq), size
+
+def generate_loaders():
+    train_set = GeneralSurgeryToolsDataset()
+    batch_size = 30
+    validation_split = .5
+    shuffle_dataset = True
+    random_seed= 42
+
+    train_set_size = int(len(train_set) * 0.5)
+    valid_set_size = len(train_set) - train_set_size
+    train_set, valid_set = random_split(train_set, [train_set_size, valid_set_size])
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
+    validation_loader = torch.utils.data.DataLoader(valid_set, batch_size=batch_size)
+    
+    mean, std, size= calc_mean_std(train_loader)
+
+    return {'train_loader':train_set,'validation_loader':valid_set,'mean':mean,'std':std,'size':size}
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 import os
-from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
+from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler,ConcatDataset
 import glob
 import cv2
 from tqdm import tqdm
@@ -9,3 +9,61 @@ import sys
 sys.path.insert(0, "/home/maria/Desktop/t-simcne")
 from tsimcne.tsimcne import TSimCNE
 from tsimcne.imagedistortions import TransformedPairDataset, get_transforms
+from loader import generate_loaders
+from tqdm import tqdm
+
+dct=generate_loaders()
+
+train_loader=dct['train_loader']
+validation_loader=dct['validation_loader']
+mean=dct['mean']
+std=dct['std']
+size=dct['size']
+
+dataset_full = torch.utils.data.ConcatDataset(
+    [train_loader, validation_loader]
+)
+
+# data augmentations for contrastive training
+transform = get_transforms(
+    mean,
+    std,
+    size=size,
+    setting="contrastive",
+)
+# transform_none just normalizes the sample
+transform_none = get_transforms(
+    mean,
+    std,
+    size=size,
+    setting="test_linear_classifier",
+)
+
+# datasets that return two augmented views of a given datapoint (and label)
+dataset_contrastive = TransformedPairDataset(train_loader, transform)
+dataset_visualize = TransformedPairDataset(dataset_full, transform_none)
+
+# wrap dataset into dataloader
+train_dl = torch.utils.data.DataLoader(
+    dataset_contrastive, batch_size=30, shuffle=True
+)
+orig_dl = torch.utils.data.DataLoader(
+    dataset_visualize, batch_size=30, shuffle=False
+)
+
+import time
+
+start=time.time()
+# create the object
+tsimcne = TSimCNE(total_epochs=[500, 50, 250])
+# train on the augmented/contrastive dataloader (this takes the most time)
+tsimcne.fit(train_dl)
+# fit the original images
+Y, labels = tsimcne.transform(orig_dl)
+end=time.time()
+
+print('Time to train: ', end-start)
+import pickle
+with open('filename.pickle', 'wb') as handle:
+    pickle.dump({'Y':Y,'labels':labels}, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
